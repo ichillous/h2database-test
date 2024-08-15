90f9368a26e142910d34f693e42af7a3cd03fe90
Isiah Chillous <44874147+ichillous@users.noreply.github.com>
2024-08-15 11:31:21 -0400

    added security features

--- src/main/java/org/ichillous/tests/h2test/config/SecurityConfig.java
+++ src/main/java/org/ichillous/tests/h2test/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package org.ichillous.tests.h2test.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,11 +14,15 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers("/api/books").permitAll()
                         .anyRequest()
                         .authenticated()
                 )
+                .headers(headers -> headers.frameOptions((frameOptions) -> frameOptions.disable()))
+                .httpBasic(Customizer.withDefaults())
                 .build();
     }
 }
